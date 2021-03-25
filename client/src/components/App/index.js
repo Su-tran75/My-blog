28c03ae44cab32613a1ba877421ee16d5abda944
su-tran75 <su-tran@hotmail.fr>
2021-03-25 05:00:53 +0100

    Initilization with the react-modele

---
+++ client/src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>My blog</h1>
+  </div>
+);
+
+// == Export
+export default App;
